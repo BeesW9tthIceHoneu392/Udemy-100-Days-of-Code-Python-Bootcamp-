61408f1f862374903f41272daddda3667ea49821
BeesW9tthIceHoneu392 <ishmam555@hotmail.com>
2026-01-14 01:15:28 -0700

    Create snake.py

---
+++ Snake_Find_Food_Game/snake.py
@@ -0,0 +1,39 @@
+
+class Snake:
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+
+
+    def create_snake(self):
+        for position in starting_position:  # makes snake body (3 classes of snakes put together)
+            new_segment = Turtle("square")
+            new_segment.color("white")
+            new_segment.penup()
+            new_segment.goto(position)
+            self.segments.append(new_segment)  # keeps created turtles in a list
+
+    def move_snake(self):
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+        self.segments[0].forward(Move_Forward)
+
+    def up(self):
+        if self.head.heading() != down:
+            self.head.setheading(up)
+
+
+    def down(self):
+        if self.head.heading() != up:
+            self.head.setheading(down)
+
+    def right(self):
+        if self.head.heading() != left:
+            self.head.setheading(right)
+
+    def left(self):
+        if self.head.heading() != right:
+             self.head.setheading(left)
